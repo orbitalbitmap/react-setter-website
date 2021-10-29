3caa15ecee3414a5c2927cc82dd717397ec1eef6
orbitalbitmap <orbitalbitmap@gmail.com>
2021-10-28 22:00:29 -0400

    Add onChange and onSubmit handlers to the NewGymFrom component

--- src/components/admin/NewGymForm.js
+++ src/components/admin/NewGymForm.js
@@ -1,6 +1,53 @@
 import React from 'react'
 
 class NewGymForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      gym: {
+        name: '',
+        address: '',
+        phoneNumber: '',
+        headSetterId: 0,
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        employees: [
+          {
+            id: 1,
+            firstName: 'Robert',
+            lastName: 'Perron',
+          }, {
+            id: 2,
+            firstName: 'Kyle',
+            lastName: 'Birnbaum',
+          },
+        ]
+      }
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      gym: {
+        ...this.state.gym,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.state.gym.headSetterId !== 0
+      ? console.log(this.state.gym)
+      : window.alert('Please select a head setter.')
+  }
+
   render() {
     return ( 
       <>
@@ -8,26 +55,33 @@ class NewGymForm extends React.Component {
 
         <form action="/api/updateGymInfo" method="post" id="editable-gym-form">
           <div className="employee-form-grid">
-            <input className="hidden" name="id" value="" />
-            <input className="hidden" name="name" value="" />
+            <label htmlFor="name">Name:</label>
+            <input onChange={this.handleChange} name="name" value={this.state.gym.name} />
             <label htmlFor="address">Address:</label>
-            <input name="address" value="" />
+            <input onChange={this.handleChange} name="address" value={this.state.gym.address} />
             <label htmlFor="phoneNumber">Phone Number:</label>
-            <input name="phoneNumber" value="" />
+            <input onChange={this.handleChange} name="phoneNumber" value={this.state.gym.phoneNumber} />
             
-            {/* For selecting a head setter */}
-            {/* //- label(htmlFor="headSetterId") Head Setter:
-            //- select(name="headSetterId")
-            //-   each employee in  gym.employees
-            //-     option(value=employee.id selected= employee.id === gym.headSetterId ? true : false)= `${employee.firstName} ${employee.lastName}` */}
+            <label htmlFor="headSetterId">Head Setter:</label>
+            <select onChange={this.handleChange} name="headSetterId" defaultValue={0}>
+            <option value={0}>Please select a setter...</option>
+              
+              {
+                this.state.gym.employees.map(employee => {
+                  return (    
+                    <option key={employee.id} value={employee.id}>{`${employee.firstName} ${employee.lastName}`}</option>
+                  )
+                })
+              }
+            </select>
             <label htmlFor="facebook">Facebook Page:</label>
-            <input name="facebook" value="" />
+            <input onChange={this.handleChange} name="facebook" value={this.state.gym.facebook} />
             <label htmlFor="instagram">Instagram Account:</label>
-            <input name="instagram" value="" />
+            <input onChange={this.handleChange} name="instagram" value={this.state.gym.instagram} />
             <label htmlFor="twitter">Twitter Account:</label>
-            <input name="twitter" value="" />
+            <input onChange={this.handleChange} name="twitter" value={this.state.gym.twitter} />
           </div>
-          <button type="submit">Update Info</button>
+          <button onClick={this.handleSubmit} type="submit">Update Info</button>
         </form>
       </>
     )
