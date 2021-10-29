0728a035670066825a6320616eb8110b7066cc0c
orbitalbitmap <orbitalbitmap@gmail.com>
2021-10-29 12:36:03 -0400

    Refactor the Login component and give it onChange and onSubmit event handlers

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,72 @@
 import React from 'react'
 
-const Login = (props) => {
-  return props.options.map(item => {
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+      options: this.props.options,
+      buttonProperties: [
+        {
+          key: "login",
+          text: "Login",
+          type: "submit",
+        },
+      ],
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state.user)
+  }
+
+  
+  render() {
     return (
-      <div key={item.name} className="employee-form-grid">
-        <label htmlFor={item?.name} form={item?.form}>{`${item?.placeholder}:`}</label>
-        <input 
-          id={item.id ? item.id : null}
-          className={item.className ? item.className : null}
-          defaultValue={item.defaultValue ? item.defaultValue : null}
-          disabled={item.isDisabled ? item.isDisabled : null}
-          form={item.form ? item.form : null}
-          name={item.name ? item.name : null}
-          placeholder={item.placeholder ? item.placeholder : null}
-          required
-          type={item.type ? item.type : null}
-        />
-      </div>
+      <form id="employee-form" action="/login" method="GET">
+        <div className="employee-form-grid">
+          <label htmlFor="email" form="employee-form" >Email:</label>
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            placeholder="Email"
+            required
+            value={this.state.user.email}
+          />
+          
+          <label htmlFor="password" form="employee-form" >Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            placeholder="Password"
+            required
+            value={this.state.user.password}
+          />
+        </div>
+
+        <button onClick={this.handleSubmit} type="submit">Login</button>
+      </form>
     )
-  })
+  }
 }
 
 export default Login
