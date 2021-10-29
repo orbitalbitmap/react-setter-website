1a1965a876a757399f5501eb9beb01a00581cd4e
orbitalbitmap <orbitalbitmap@gmail.com>
2021-10-28 21:16:26 -0400

    Add onChange handlers to the EditSingleGym component

--- src/components/gyms/EditSingleGym.js
+++ src/components/gyms/EditSingleGym.js
@@ -1,64 +1,87 @@
 import React from 'react'
 
-const gym = {
-  id: 1,
-  name:'Worcester',
-  address: '299 Barber Avenue Worcester, Massachusetts',
-  phoneNumber: '555-666-7777',
-  headSetterId: 2,
-  facebook: '',
-  instagram: '',
-  twitter: '',
-  employees: [
-    {
-      id: 1,
-      firstName: 'Robert',
-      lastName: 'Perron',
-    }, {
-      id: 2,
-      firstName: 'Kyle',
-      lastName: 'Birnbaum',
-    },
-  ]
-}
+class EditSingleGym extends React.Component {
+  constructor(props) {
+    super(props)
 
-
-  class EditSingleGym extends React.Component {
-    render() {
-      return (
-        <>
-          <h1 className="centered-text">{`Edit ${gym.name}'s Gym Information`}</h1>
-
-          <form action="/api/updateGymInfo" method="post" id="editable-gym-form">
-            <div className="employee-form-grid">
-              <input className="hidden" name="id" value={gym.id} />
-              <input className="hidden" name="name" value={gym.name} />
-              <label htmlFor="address">Address:</label>
-              <input name="address" value={gym.address} />
-              <label htmlFor="phoneNumber">Phone Number:</label>
-              <input name="phoneNumber" value={gym.phoneNumber} />
-              <label htmlFor="headSetterId">Head Setter:</label>
-              <select name="headSetterId" defaultValue={gym.headSetterId}>
-                {
-                  gym.employees.map(employee => {
-                    return (
-                      <option key={employee.id} value={employee.id }>{`${employee.firstName} ${employee.lastName}`}</option>
-                    )
-                  })
-                }
-              </select>
-              <label htmlFor="facebook">Facebook Page:</label>
-              <input name="facebook" value={gym.facebook === null ? '' : gym.facebook} />
-              <label htmlFor="instagram">Instagram Account:</label>
-              <input name="instagram" value={gym.instagram === null ? '' : gym.instagram} />
-              <label htmlFor="twitter">Twitter Account:</label>
-              <input name="twitter" value={gym.twitter === null ? '' : gym.twitter} />
-            </div>
-            <button type="submit">Update Info</button>
-          </form>
-        </>
-      )
+    this.state = {
+      gym: {
+        id: 1,
+        name:'Worcester',
+        address: '299 Barber Avenue Worcester, Massachusetts',
+        phoneNumber: '555-666-7777',
+        headSetterId: 2,
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        employees: [
+          {
+            id: 1,
+            firstName: 'Robert',
+            lastName: 'Perron',
+          }, {
+            id: 2,
+            firstName: 'Kyle',
+            lastName: 'Birnbaum',
+          },
+        ]
+      }
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
   }
+
+  handleChange(event) {
+    this.setState({
+      gym: {
+        ...this.state.gym,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state.gym)
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="centered-text">{`Edit ${this.state.gym.name}'s Gym Information`}</h1>
+
+        <form action="/api/updateGymInfo" method="post" id="editable-gym-form">
+          <div className="employee-form-grid">
+            <input className="hidden" name="id" value={this.state.gym.id} disabled />
+            <input className="hidden" name="name" value={this.state.gym.name} disabled />
+            <label htmlFor="address">Address:</label>
+            <input name="address" onChange={this.handleChange} value={this.state.gym.address} />
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <input name="phoneNumber" onChange={this.handleChange} value={this.state.gym.phoneNumber} />
+            <label htmlFor="headSetterId">Head Setter:</label>
+            <select name="headSetterId"  onChange={this.handleChange} defaultValue={this.state.gym.headSetterId}>
+              {
+                this.state.gym.employees.map(employee => {
+                  return (
+                    <option key={employee.id} value={employee.id }>{`${employee.firstName} ${employee.lastName}`}</option>
+                  )
+                })
+              }
+            </select>
+            <label htmlFor="facebook">Facebook Page:</label>
+            <input name="facebook"  onChange={this.handleChange} value={this.state.gym.facebook === null ? '' : this.state.gym.facebook} />
+            <label htmlFor="instagram">Instagram Account:</label>
+            <input name="instagram"  onChange={this.handleChange} value={this.state.gym.instagram === null ? '' : this.state.gym.instagram} />
+            <label htmlFor="twitter">Twitter Account:</label>
+            <input name="twitter"  onChange={this.handleChange} value={this.state.gym.twitter === null ? '' : this.state.gym.twitter} />
+          </div>
+          <button onClick={this.handleSubmit} type="submit">Update Info</button>
+        </form>
+      </>
+    )
+  }
+}
 
   export default EditSingleGym
