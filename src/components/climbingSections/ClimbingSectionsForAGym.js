b1d51e7a32a1f6e9da1eabad5e44ea1ae295ebb9
orbitalbitmap <orbitalbitmap@gmail.com>
2021-11-19 08:07:27 -0500

    Dry up the rendering of sections and remove hardcoded values for API calls

--- src/components/climbingSections/ClimbingSectionsForAGym.js
+++ src/components/climbingSections/ClimbingSectionsForAGym.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 
 const gym = {
@@ -37,20 +38,44 @@ const gym = {
 }
 
 class ClimbingSectionsForAGym extends React.Component {
-  render(){
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      gym: []
+    }
+
+    this.renderSections = this.renderSections.bind(this)
+  }
+
+  renderSections(sections, type) {
+    return sections.map(section => {
+      return (
+        <li key={`${type}-sections-${section.id}`} className="centered-text inside-bullet">{section.name}</li>
+      )
+  })
+  }
+
+  async componentDidMount() {
+    const { data } = await axios.get('http://localhost:1337/api/gymWithSections/1')
+
+    this.setState({
+      gym: data
+    })
+  }
+
+  render() {
     return (
       <>
-        <h1 className="centered-text">{gym.name}</h1>
+        <h1 className="centered-text">{this.state.gym.name}</h1>
         <div className="location-grid">
           <div className="boulder-locations">
             <h3 className="centered-text">Boulder sections</h3>
             <ul>
               {
-                gym.boulders.map(section => {
-                    return (
-                      <li key={`boulder-${section.id}`} className="centered-text inside-bullet">{section.name}</li>
-                    )
-                })
+                this.state.gym.boulderSections
+                  ? this.renderSections(this.state.gym.boulderSections, 'boulder')
+                  : <p>No boulder sections found.</p>
               }
               </ul>
           </div>
@@ -59,11 +84,9 @@ class ClimbingSectionsForAGym extends React.Component {
             <h3 className="centered-text">Rope sections</h3>
             <ul>
               {
-                gym.ropes.map(section => {
-                    return (
-                      <li key={`rope-${section.id}`} className="centered-text inside-bullet">{section.name}</li>
-                    )
-                })
+                this.state.gym.routeSections
+                ? this.renderSections(this.state.gym.routeSections, 'route')
+                : <p>No route sections found.</p>
               }
               </ul>
           </div>
