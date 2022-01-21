9440595cf55c62a6acf68b97c957ebad0c3e07e0
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-21 13:08:59 -0500

    Change the inital state of the redux state user variable to null

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from "../actions/types"
 
-const signIn = (state = {}, action) => {
+const signIn = (state = null, action) => {
   switch (action.type) {
     case SIGN_IN:
       return action.payload
