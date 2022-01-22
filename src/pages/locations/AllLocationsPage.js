01a648cd956bf55088c3da3525e266bf90ebd6e4
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 16:47:50 -0500

    Refactor the AllLocationsPage component from a class component to a functional component

--- src/pages/locations/AllLocationsPage.js
+++ src/pages/locations/AllLocationsPage.js
@@ -1,40 +1,18 @@
-import axios from 'axios'
 import React from 'react'
 import { connect } from 'react-redux'
 
 import Gyms from '../../components/locations/Gyms'
 import Navbar from '../../components/navbar/Navbar'
 
-
-class AllLocationsPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: {}
-    }
-  }
-
-  async componentDidMount() {
-    const user = (await axios.get('http://localhost:1337/api/employees/1')).data
-    const gyms = (await axios.get('http://localhost:1337/api/allGymSections')).data
-    this.setState({
-      gyms,
-      user
-    })
-  }
-
-
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className="centered-text">
-          {this.props.gyms ? <Gyms /> : null}
-        </div>
-      </>
-    )
-  }
+const AllLocationsPage = (props) => {
+  return (
+    <>
+      <Navbar />
+      <div className="centered-text">
+        {props.gyms ? <Gyms /> : null}
+      </div>
+    </>
+  )
 }
 
 const mapStateToProps = (state) => {
