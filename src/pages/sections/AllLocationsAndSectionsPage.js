c5728ca56395e7b1e5796db0632bc1001578e10e
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 17:07:17 -0500

    Refactor the AllLocationsAndSectionsPage from a class component to a functional component

--- src/pages/sections/AllLocationsAndSectionsPage.js
+++ src/pages/sections/AllLocationsAndSectionsPage.js
@@ -5,17 +5,15 @@ import AllClimbingSections from '../../components/sections/AllClimbingSections'
 import Navbar from '../../components/navbar/Navbar'
 
 
-class AllLocationsAndSections extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className="centered-text">
-          {this.props.gyms ? <AllClimbingSections /> : null}
-        </div>
-      </>
-    )
-  }
+const AllLocationsAndSections = (props) => {
+  return (
+    <>
+      <Navbar />
+      <div className="centered-text">
+        {props.gyms ? <AllClimbingSections /> : null}
+      </div>
+    </>
+  )
 }
 
 const mapStateToProps = (state) => {
