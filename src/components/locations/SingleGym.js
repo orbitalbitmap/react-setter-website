614e88e75b74790e22242a9dd2eee0ee8d040f13
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 13:13:45 -0500

    Refactor the SingleGym component from a class component to a functional component

--- src/components/locations/SingleGym.js
+++ src/components/locations/SingleGym.js
@@ -1,83 +1,83 @@
 import axios from 'axios'
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-class SingleGym extends React.Component {
-  constructor(props) {
-    super(props)
+const SingleGym = (props) => {
+  const urlParams = useParams()
 
-    this.state = {
-      employeeList: [],
-      fullTimeEmployeeList: [],
-      gymInfo: [],
-      headSetter: {},
-      partTimeEmployeeList: [],
-    }
-  }
+      const [fullTimeEmployeeList, setFullTimeEmployeeList] = useState([])
+      const [gymInfo, setGymInfo] = useState([])
+      const [headSetter, setHeadSetter] = useState({})
+      const [partTimeEmployeeList, setPartTimeEmployeeList] = useState([])
 
-  async componentDidMount() {
-    const { data } = await axios.get('http://localhost:1337/api/gyms/worcester')
+  useEffect( () => {
+    const getInfo = async () => {
+      const fetchedInfo = (await axios.get(`http://localhost:1337/api/gymById/${urlParams.id}`)).data
+        setGymInfo(fetchedInfo)
+      }
 
-    const employeeList = data.employees
-    const headSetter = employeeList.find(employee => employee.id === data.headSetterId)
-    const fullTimeEmployeeList = employeeList.filter(employee => employee.roleId <= 4 && employee.id !== data.headSetterId && employee.id !== 1)
-    const partTimeEmployeeList = employeeList.filter(employee => employee.roleId === 5)
+    getInfo()
+    }, [urlParams])
 
-    this.setState({
-      employeeList,
-      fullTimeEmployeeList,
-      gymInfo: data,
-      headSetter,
-      partTimeEmployeeList,
-    })
-  }
+    useEffect(() => {
+      setHeadSetter(gymInfo?.employees?.find(employee => employee.id === gymInfo.headSetterId))
+      setFullTimeEmployeeList(gymInfo?.employees?.filter(employee => employee.roleId <= 4 && employee.id !== gymInfo.headSetterId && employee.id !== 1))
+      setPartTimeEmployeeList(gymInfo?.employees?.filter(employee => employee.roleId === 5))
+    }, [gymInfo.employees, gymInfo.headSetterId])
 
-  render() {
-    return (
-      <>
-        <h1 className="centered-text">{this.state.gymInfo.name}</h1>
-        <h3 className="centered-text">{this.state.gymInfo.address}</h3>
-        <h3 className="centered-text">{this.state.gymInfo.phoneNumber}</h3>
-        <h3 className="centered-text">{`Facebook: ${this.state.gymInfo.facebook !== null ? this.state.gymInfo.facebook : 'None available'}`}</h3>
-        <h3 className="centered-text">{`Instagram: ${this.state.gymInfo.instagram !== null ? this.state.gymInfo.instagram : 'None available'}`}</h3>
-        <h3 className="centered-text">{`Twitter: ${this.state.gymInfo.twitter !== null ? this.state.gymInfo.twitter : 'None available'}`}</h3>
+    console.log('rendered')
+      return (
+        <>
+          <h1 className="centered-text">{gymInfo.name}</h1>
+          <h3 className="centered-text">{gymInfo.address}</h3>
+          <h3 className="centered-text">{gymInfo.phoneNumber}</h3>
+          <h3 className="centered-text">{`Facebook: ${gymInfo.facebook !== null ? gymInfo.facebook : 'None available'}`}</h3>
+          <h3 className="centered-text">{`Instagram: ${gymInfo.instagram !== null ? gymInfo.instagram : 'None available'}`}</h3>
+          <h3 className="centered-text">{`Twitter: ${gymInfo.twitter !== null ? gymInfo.twitter : 'None available'}`}</h3>
 
-        <h2 className="centered-text">The Setters:</h2>
-        <h3 className="centered-text">Head Setter:</h3>
-        <ul className="centered-text">
-          <li key={this.state.headSetter.id} className="centered-text inside-bullet">
-            <a href={`/employees/${this.state.headSetter.id}`}>{`${this.state.headSetter.firstName} ${this.state.headSetter.lastName}`}</a>
-          </li>
-        </ul>
-        <h3 className="centered-text">Full Time Setters</h3>
-        <ul>
+          <h2 className="centered-text">The Setters:</h2>
+          <h3 className="centered-text">Head Setter:</h3>
+          <ul className="centered-text">
+            <li key={headSetter?.id} className="centered-text inside-bullet">
+              <a href={`/employees/${headSetter?.id}`}>{`${headSetter?.firstName} ${headSetter?.lastName}`}</a>
+            </li>
+          </ul>
+          <h3 className="centered-text">Full Time Setters</h3>
+          <ul>
+            {
+              fullTimeEmployeeList?.map(setter => {
+                return (
+                  <li key={`full-time-${setter?.id}`} className="centered-text inside-bullet">
+                    <a href={`/employees/${setter?.id}`}>{`${setter?.firstName} ${setter?.lastName}`}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+
+          <h3 className="centered-text">Part Time Setters</h3>
+          <ul>
           {
-            this.state.fullTimeEmployeeList.map(setter => {
-              return (
-                <li key={`full-time-${setter.id}`} className="centered-text inside-bullet">
-                  <a href={`/employees/${setter.id}`}>{`${setter.firstName} ${setter.lastName}`}</a>
-                </li>
-              )
-            })
-          }
-        </ul>
+              partTimeEmployeeList?.map(setter => {
+                return (
+                  <li key={`part-time-${setter?.id}`} className="centered-text inside-bullet">
+                    <a href={`/employees/${setter?.id}`}>{`${setter?.firstName} ${setter?.lastName}`}</a>
+                  </li>
+                )
+              })
+            }
+          </ul>
 
-        <h3 className="centered-text">Part Time Setters</h3>
-        <ul>
-        {
-            this.state.partTimeEmployeeList.map(setter => {
-              return (
-                <li key={`part-time-${setter.id}`} className="centered-text inside-bullet">
-                  <a href={`/employees/${setter.id}`}>{`${setter.firstName} ${setter.lastName}`}</a>
-                </li>
-              )
-            })
-          }
-        </ul>
+          <a href={`/admin/location/${gymInfo?.id}`}>Edit Gym info</a>
+        </>
+      )
+  }
 
-        <a href={`/admin/location/${this.state.gymInfo.id}`}>Edit Gym info</a>
-      </>
-    )
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user,
   }
 }
 
-export default SingleGym
+export default connect(mapStateToProps, {})(SingleGym)
