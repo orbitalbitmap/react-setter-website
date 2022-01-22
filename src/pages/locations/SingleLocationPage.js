8fb9470fc44bfb96e619d7b3b3584ff75326332c
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 16:27:30 -0500

    Implement redux state to the SingleLocationPage component

--- src/pages/locations/SingleLocationPage.js
+++ src/pages/locations/SingleLocationPage.js
@@ -1,39 +1,28 @@
-import axios from 'axios'
 import React from 'react'
+import { connect } from 'react-redux'
 
 import SingleGym from '../../components/locations/SingleGym'
 import Navbar from '../../components/navbar/Navbar'
 
 
 class SingleLocationPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: {}
-    }
-  }
-
-  async componentDidMount() {
-    const user = (await axios.get('http://localhost:1337/api/employees/1')).data
-    const gyms = (await axios.get('http://localhost:1337/api/allGymSections')).data
-    this.setState({
-      gyms,
-      user
-    })
-  }
-
-
   render() {
     return (
       <>
-        <Navbar user={this.state.user} gyms={this.state.gyms} />
+        <Navbar user={this.props.user} gyms={this.props.gyms} />
         <div className="centered-text">
-          {this.state.gyms ? <SingleGym /> : null}
+          {this.props.gyms ? <SingleGym /> : null}
         </div>
       </>
     )
   }
 }
 
-export default SingleLocationPage
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    gyms: state.gyms,
+  }
+}
+
+export default connect(mapStateToProps, {})(SingleLocationPage)
