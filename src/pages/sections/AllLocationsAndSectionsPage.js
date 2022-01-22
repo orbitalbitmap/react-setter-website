433bbe9f8201f4d6c3223143aa9e670fc38f5592
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 17:01:52 -0500

    Implement redux state into AllLocationsAndSectionsPage component

--- src/pages/sections/AllLocationsAndSectionsPage.js
+++ src/pages/sections/AllLocationsAndSectionsPage.js
@@ -1,39 +1,28 @@
-import axios from 'axios'
 import React from 'react'
+import { connect } from 'react-redux'
 
-import AllClimbingSections from '../../components/climbingSections/AllClimbingSections'
+import AllClimbingSections from '../../components/sections/AllClimbingSections'
 import Navbar from '../../components/navbar/Navbar'
 
 
 class AllLocationsAndSections extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: {}
-    }
-  }
-
-  async componentDidMount() {
-    const user = (await axios.get('http://localhost:1337/api/employees/1')).data
-    const gyms = (await axios.get('http://localhost:1337/api/allGymSections')).data
-    this.setState({
-      gyms,
-      user
-    })
-  }
-
-
   render() {
     return (
       <>
-        <Navbar user={this.state.user} gyms={this.state.gyms} />
+        <Navbar />
         <div className="centered-text">
-          {this.state.gyms ? <AllClimbingSections /> : null}
+          {this.props.gyms ? <AllClimbingSections /> : null}
         </div>
       </>
     )
   }
 }
 
-export default AllLocationsAndSections
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    gyms: state.gyms,
+  }
+}
+
+export default connect(mapStateToProps, {})(AllLocationsAndSections)
