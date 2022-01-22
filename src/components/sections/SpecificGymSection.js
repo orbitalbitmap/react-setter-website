64a50e2cf8e89c689f7f2a561e7b9051abd8f3ab
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 18:51:03 -0500

    Refactor the SpecificGymSection class component to a funtional component

--- src/components/sections/SpecificGymSection.js
+++ src/components/sections/SpecificGymSection.js
@@ -1,75 +1,60 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-class SpecificGymSection extends React.Component {
-  constructor(props) {
-    super(props)
+const SpecificGymSection = () => {
+  const [gym, setGym] = useState({})
+  const [selectedClimbType, setSelectedClimbType] = useState('Boulder')
+  const [selectedSectionList, setSelectedSectionList] = useState([])
 
-    this.state = {
-      selectedClimbType: 'Boulder',
-      gym: {}, 
-      sections: []
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const { data } = await axios.get('http://localhost:1337/api/gymWithSections/1')
+
+      setGym(data)
     }
 
-    this.handleChange = this.handleChange.bind(this)
-  }
-  
-  handleChange(event) {
-    const { gym } = this.state
-    
-    const selectedSectionList = event.target.value === 'Boulder'
-    ? gym.boulderSections
-    : gym.routeSections
-    
+    getInfo()
+  }, [])
 
-    this.setState({
-      selectedClimbType: event.target.value,
-      sections: selectedSectionList,
-    })
-  }
+  useEffect(() => {
+    selectedClimbType === 'Boulder' 
+      ? setSelectedSectionList(gym.boulderSections)
+      : setSelectedSectionList(gym.routeSections)
 
-  async componentDidMount() {
-    const { data } = await axios.get('http://localhost:1337/api/gymWithSections/1')
+  }, [gym, selectedClimbType])
 
-    this.setState({
-      gym: data,
-      sections: data.boulderSections
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <h1 className="centered-text">{this.state.gym.name }</h1>
-        <div className="centered-text">
-          <select onChange={this.handleChange} value={this.state.selectedClimbType}>
-            <option value="Boulder">Boulder Sections</option>
-            <option value="Route">Route Sections</option>
-          </select>
+  return (
+    <>
+      <h1 className="centered-text">{gym.name }</h1>
+      <div className="centered-text">
+        <select onChange={event => setSelectedClimbType(event.target.value)} value={selectedClimbType}>
+          <option value="Boulder">Boulder Sections</option>
+          <option value="Route">Route Sections</option>
+        </select>
+      </div>
+      <div className="location-grid">
+        <div className={`${selectedClimbType || 'Unavailable'}-locations`}>
+          <h3 className="centered-text">{`${selectedClimbType || 'Unavailable'} sections`}</h3>
+          <ul>
+            {
+              selectedSectionList?.map(section => {
+                return (
+                  <li key={section.id} className="centered-text inside-bullet">{section.name}</li>
+                )
+              })
+            }
+          </ul>
         </div>
-        <div className="location-grid">
-          <div className={`${this.state.climbType}-locations`}>
-            <h3 className="centered-text">{`${this.state.selectedClimbType} sections`}</h3>
-            <ul>
-              {
-                this.state.sections.map(section => {
-                  return (
-                    <li key={section.id} className="centered-text inside-bullet">{section.name}</li>
-                  )
-                })
-              }
-            </ul>
-          </div>
 
-          <div>
-            <h3 className="centered-text">
-              <a href={`/gymSections/edit/${this.state.gym.id}`}>Edit All Section Names</a>
-            </h3>
-          </div>
+        <div>
+          <h3 className="centered-text">
+            <a href={`/gymSections/edit/${gym.id}`}>Edit All Section Names</a>
+          </h3>
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
 }
 
 export default SpecificGymSection
