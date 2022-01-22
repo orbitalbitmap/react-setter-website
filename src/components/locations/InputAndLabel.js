824263ec0c123a67c9e387a47e39bd99996d4d9b
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-22 16:22:55 -0500

    Refactor the InputAndLabel component from a class component to a functional component

--- src/components/locations/InputAndLabel.js
+++ src/components/locations/InputAndLabel.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
-class InputAndLabel extends React.Component {
-  
-  render() {
-    return (
-      <>
-        <label htmlFor={this.props.name}>{this.props.text}:</label>
-        <input name={this.props.name} onChange={this.props.handleChange} value={this.props.value} />
-      </>
-    )
-  }
+const InputAndLabel = (props) => {
+  return (
+    <>
+      <label htmlFor={props.name}>{props.text}:</label>
+      <input name={props.name} onChange={props.handleChange} value={props.value} />
+    </>
+  )
 }
 
 export default InputAndLabel
