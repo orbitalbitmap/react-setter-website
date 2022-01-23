20833d270482b35a650f25c4bdde494e343fd626
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-23 18:44:22 -0500

    Refactor the LoginPage class component to a functional one and remove unecessary logic

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,40 +1,12 @@
-import axios from 'axios'
-import React from 'react'
-import { Cookies } from 'react-cookie'
-
 import '../components/styles.css'
 import Login from '../components/Login'
 
-class LoginPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: {},
-      gyms: [], 
-      cookies: new Cookies()
-    }
-  }
-
-  async componentDidMount() {
-    if (this.state.cookies.get('setterLoggedIn')) {
-      window.location.href = "/dashboard"
-    }
-
-    const gyms = (await axios.get('http://localhost:1337/api/gyms')).data
-    
-    this.setState({
-      gyms: gyms,
-    })
-  }
-  
-  render() {
-    return (
-      <div>
-        <Login />
-      </div>
-    )
-  }
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
 }
 
 export default LoginPage
