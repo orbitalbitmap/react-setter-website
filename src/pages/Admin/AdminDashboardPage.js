48004d238fea8ea964024640c80993ca65470ad6
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-23 21:49:32 -0500

    Refactor to use the AdminDashboard component

--- src/pages/Admin/AdminDashboardPage.js
+++ src/pages/Admin/AdminDashboardPage.js
@@ -1,19 +1,11 @@
+import AdminDashboard from '../../components/admin/AdminDashboard'
 import Navbar from '../../components/navbar/Navbar'
 
 const AdminPage = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <ul>
-          <li>
-            <a href="/admin/employee/new">New Employee</a>
-          </li>
-          <li>
-            <a href="/admin/location/new">New Location</a>
-          </li>
-        </ul>
-      </div>
+      <AdminDashboard />
     </>
   )
 }
