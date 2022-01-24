acd809282e128aabf51b3de8b70b99ff1ed400ea
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-23 21:31:29 -0500

    Refactor the AdminDashboard class component to a functional component

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -1,22 +1,18 @@
-import React from 'react'
-
-class AdminDashboard extends React.Component {
-  render() {
-    return (
-      <div className="centered-text">
-        <h3>
-          <ul>
-            <li style={{listStyle: 'none'}}>
-              <a href="/newEmployee">Add new setter</a>
-            </li>
-            <li style={{listStyle: 'none'}}>
-              <a href="/admin/newGym">Add new gym</a>
-            </li>
-          </ul>
-        </h3>
-      </div>
-    )
-  }
+const AdminDashboard = () => {
+  return (
+    <div className="centered-text">
+      <h3>
+        <ul>
+          <li style={{listStyle: 'none'}}>
+            <a href="/newEmployee">Add new setter</a>
+          </li>
+          <li style={{listStyle: 'none'}}>
+            <a href="/admin/newGym">Add new gym</a>
+          </li>
+        </ul>
+      </h3>
+    </div>
+  )
 }
 
 export default AdminDashboard
