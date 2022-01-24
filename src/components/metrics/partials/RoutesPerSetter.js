03939d234f5d2669fcca0fe261884f8709508239
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-23 19:44:38 -0500

    Refactor the RoutesPerSetter class component to a functional component

--- src/components/metrics/partials/RoutesPerSetter.js
+++ src/components/metrics/partials/RoutesPerSetter.js
@@ -1,41 +1,37 @@
-import React from 'react'
 import { Bar } from 'react-chartjs-2'
 
-class RoutesPerSetter extends React.Component {
-  render() {
-    const chartData = {
-      labels: Object.keys(this.props.data) || null,
-      datasets: [
-        {
-          data: Object.values(this.props.data) || null,
-          label: 'Total Routes',
-          backgroundColor: 'black',
-          fill: false
-        },
-      ]
-    }
-
-    return (
-        <div>
-          <Bar
-            data={chartData}
-            options={{
-              plugins: {
-                title: {
-                  display: true,
-                  text: "Routes Per Setter"
-                },
-                legend: {
-                  display: true,
-                  position: "bottom"
-                }
-              }
-            }}
-          />
-      </div>
-    )
+const RoutesPerSetter = (props) => {
+  const chartData = {
+    labels: Object.keys(props.data) || null,
+    datasets: [
+      {
+        data: Object.values(props.data) || null,
+        label: 'Total Routes',
+        backgroundColor: 'black',
+        fill: false
+      },
+    ]
   }
-}
 
+  return (
+      <div>
+        <Bar
+          data={chartData}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Routes Per Setter"
+              },
+              legend: {
+                display: true,
+                position: "bottom"
+              }
+            }
+          }}
+        />
+    </div>
+  )
+}
 
 export default RoutesPerSetter
