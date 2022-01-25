eaf4e795b6aba8b94052501e46023ff3f4f8aa62
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-25 13:04:20 -0500

    Refactor the AdminUpdateEmployeePage class component to a functional component

--- src/pages/Admin/AdminUpdateEmployeePage.js
+++ src/pages/Admin/AdminUpdateEmployeePage.js
@@ -1,39 +1,16 @@
-import axios from 'axios'
-import React from 'react'
-
 import UpdateEmployee from '../../components/employees/UpdateEmployee'
 import Navbar from '../../components/navbar/Navbar'
 
 
-class AdminUpdateEmployeePage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      user: {}
-    }
-  }
-
-  async componentDidMount() {
-    const user = (await axios.get('http://localhost:1337/api/employees/1')).data
-    const gyms = (await axios.get('http://localhost:1337/api/allGymSections')).data
-    this.setState({
-      gyms,
-      user
-    })
-  }
-
-
-  render() {
-    return (
-      <>
-        <Navbar user={this.state.user} gyms={this.state.gyms} />
-        <div className="centered-text">
-          {this.state.gyms ? <UpdateEmployee /> : null}
-        </div>
-      </>
-    )
-  }
+const AdminUpdateEmployeePage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="centered-text">
+        <UpdateEmployee />
+      </div>
+    </>
+  )
 }
 
 export default AdminUpdateEmployeePage
