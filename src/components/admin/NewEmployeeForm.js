7d3e1b919f7b314fcbc5a392d827515f07d85d66
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-25 12:36:04 -0500

    Refactor the NewEmployee class component to a functional component and add in redux state integration

--- src/components/admin/NewEmployeeForm.js
+++ src/components/admin/NewEmployeeForm.js
@@ -1,167 +1,144 @@
-import axios from 'axios'
-import React from 'react'
+import { useState} from 'react'
+import { connect } from 'react-redux'
 
+const NewEmployeeForm = (props) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [roleId, setRoleId] = useState(0)
+  const [employeeGymList, setEmployeeGymList] = useState([])
 
-class NewEmployeeForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      newUser: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        phoneNumber: '',
-        roleId: 0,
-        employeeGymList: []
-      },
-      gyms: [],
-    }
-  
-    this.handleChange = this.handleChange.bind(this)
-    this.handleCheckbox = this.handleCheckbox.bind(this)
-    this.hanldeSubmit = this.hanldeSubmit.bind(this)
-  }
-
-  handleChange(event) {
-
-    this.setState({
-      newUser: {
-        ...this.state.newUser,
-        [event.target.name]: event.target.value
-      }
-    })
-  }
-
-  handleCheckbox(event) {
+  const handleCheckbox = (event) => {
     const id = parseInt(event.target.value)
     switch (event.target.checked) {
       case false:
-        this.setState({
-          newUser: {
-            ...this.state.newUser,
-            employeeGymList: this.state.newUser.employeeGymList.filter(gym => {
-              return gym.id !== id
-            })
-          }
+        const updatedGymList = employeeGymList.filter(gym => {
+            return gym.id !== id
         })
+
+        setEmployeeGymList(updatedGymList)
         break
       default:
-        const gymToAdd = this.state.gyms.find(gym => gym.id === id)
-        this.setState({
-          newUser: {
-            ...this.state.newUser,
-            employeeGymList: [
-              ...this.state.newUser.employeeGymList,
-              gymToAdd
-            ].sort((gymA, gymB) => gymA.id - gymB.id)
-          }
-        })
+        const gymToAdd = props.gyms.find(gym => gym.id === id)
+        const newSortedGymList = [...employeeGymList, gymToAdd].sort((gymA, gymB) => gymA.id - gymB.id)
+        
+        setEmployeeGymList(newSortedGymList)
+
     }
   }
 
-  hanldeSubmit(event) {
+  const hanldeSubmit = (event) => {
     event.preventDefault()
+
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      roleId,
+      employeeGymList,
+    }
+
+    console.log(newUser)
   }
 
-  async componentDidMount() {
-    const { data } = await axios.get('http://localhost:1337/api/gyms')
+  return (
+    <>
+      <h1 className="centered-text">New Employee Information</h1>
 
-    this.setState({
-      gyms: data
-    })
-  }
+      <form id="employee-form">
+        <div className="employee-form-grid">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            onChange={(event) => setFirstName(event.target.value)}
+            name="firstName"
+            placeholder="First name"
+            value={firstName}
+            required
+          />
 
-  render(){
-    return (
-      <>
-        <h1 className="centered-text">New Employee Information</h1>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            onChange={(event) => setLastName(event.target.value)}
+            name="lastName"
+            placeholder="Last name"
+            required
+            value={lastName}
+          />
 
-        <form action="/api/saveEmployee" method="post" id="employee-form">
-          <div className="employee-form-grid">
-            <label htmlFor="firstName">First Name:</label>
-            <input
-              onChange={this.handleChange}
-              name="firstName"
-              placeholder="First name"
-              value={this.state.newUser.firstName}
-              required
-            />
+          <label htmlFor="email">Email:</label>
+          <input
+            onChange={(event) => setEmail(event.target.value)}
+            name="email"
+            placeholder="Email"
+            required
+            value={email}
+          />
 
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              onChange={this.handleChange}
-              name="lastName"
-              placeholder="Last name"
-              required
-              value={this.state.newUser.lastName}
-            />
+          <label htmlFor="password">Password:</label>
+          <input
+            onChange={(event) => setPassword(event.target.value)}
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+          />
 
-            <label htmlFor="email">Email:</label>
-            <input
-              onChange={this.handleChange}
-              name="email"
-              placeholder="Email"
-              required
-              value={this.state.newUser.email}
-            />
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            onChange={(event) => setPhoneNumber(event.target.value)}
+            name="phoneNumber"
+            placeholder="555-555-5555"
+            value={phoneNumber}
+          />
 
-            <label htmlFor="password">Password:</label>
-            <input
-              onChange={this.handleChange}
-              name="password"
-              type="password"
-              placeholder="Password"
-              required
-              value={this.state.newUser.password}
-            />
+          <label htmlFor="roleId">Role:</label>
+          <select onChange={(event) => setRoleId(event.target.value)} name="roleId" defaultValue="0" required>
+            <option value="0">Please select a role...</option>
+            <option value="1">Director of Routesetting</option>
+            <option value="2">Regional Head Setter</option>
+            <option value="3">Head Setter</option>
+            <option value="4">Full Time Setter</option>
+            <option value="5">Part Time Setter</option>
+          </select>
+        </div>
 
-            <label htmlFor="phoneNumber">Phone Number:</label>
-            <input
-              onChange={this.handleChange}
-              name="phoneNumber"
-              placeholder="555-555-5555"
-              value={this.state.newUser.phoneNumber}
-            />
-
-            <label htmlFor="roleId">Role:</label>
-            <select onChange={this.handleChange} name="roleId" defaultValue="0" required>
-              <option value="1">Please select a role...</option>
-              <option value="1">Director of Routesetting</option>
-              <option value="2">Regional Head Setter</option>
-              <option value="3">Head Setter</option>
-              <option value="4">Full Time Setter</option>
-              <option value="5">Part Time Setter</option>
-            </select>
+        <h3 className="centered-text">Locations:</h3>
+        <div className="checkbox-grid">
+          {
+            props.gyms.map(gym => {
+              return (
+                <div key={gym.id}>
+                  <label htmlFor="gyms" form="employee-form">{gym.name}:</label>
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    form="employee-form" 
+                    name="gyms"
+                    value={gym.id}
+                    checked={employeeGymList.some(employeeGym => employeeGym.id === gym.id)}
+                    onChange={handleCheckbox}
+                  />
+                </div>
+              )
+            })
+          }
           </div>
+        <button onClick={hanldeSubmit} type="button">Save Employee</button>
+      </form>
+    </>
+  )
+}
 
-          <h3 className="centered-text">Locations:</h3>
-          <div className="checkbox-grid">
-            {
-              this.state.gyms.map(gym => {
-                return (
-                  <div key={gym.id}>
-                    <label htmlFor="gyms" form="employee-form">{gym.name}:</label>
-                    <input
-                      type="checkbox"
-                      className="checkbox"
-                      form="employee-form" 
-                      name="gyms"
-                      value={gym.id}
-                      checked={this.state.newUser.employeeGymList.some(employeeGym => employeeGym.id === gym.id)}
-                      onChange={this.handleCheckbox}
-                    />
-                  </div>
-                )
-              })
-            }
-            </div>
-          <button onClick={this.hanldeSubmit} type="submit">Save Employee</button>
-        </form>
-      </>
-    )
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    gyms: state.gyms,
   }
 }
 
-export default NewEmployeeForm
+export default connect(mapStateToProps, {})(NewEmployeeForm)
