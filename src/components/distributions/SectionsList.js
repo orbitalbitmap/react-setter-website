b66dbdf2926a2a584b12bd8675bf5dbc208b6aa7
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-26 13:48:31 -0500

    Refactor the SectionsList class component to a functional component

--- src/components/distributions/SectionsList.js
+++ src/components/distributions/SectionsList.js
@@ -1,23 +1,21 @@
 import React from 'react'
 
-class SectionsList extends React.Component {
-  render() {
-    return (
-      this.props.sectionList.map(section => {
-        return (
-          <div key={`section-${section.id}`}>
-            <span
-              id={section.id}
-              className="section-selectors"
-              onClick={this.props.onClick}
-            >
-              {section?.name}
-            </span>
-          </div>
-        )
-      })
-    )
-  }
+const SectionsList = (props) => {
+  return (
+    props.sectionList.map(section => {
+      return (
+        <div key={`section-${section.id}`}>
+          <span
+            id={section.id}
+            className="section-selectors"
+            onClick={props.onClick}
+          >
+            {section?.name}
+          </span>
+        </div>
+      )
+    })
+  )
 }
 
 export default SectionsList
