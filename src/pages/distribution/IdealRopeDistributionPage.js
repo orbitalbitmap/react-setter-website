4b707d62c7bd8853479b0c17764e074f790f7371
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-27 13:47:45 -0500

    Add a path property for the api call

--- src/pages/distribution/IdealRopeDistributionPage.js
+++ src/pages/distribution/IdealRopeDistributionPage.js
@@ -4,11 +4,12 @@ import Navbar from '../../components/navbar/Navbar'
 import DistributionEditForm from '../../components/distributions/DistributionEditForm'
 
 const IdealRopeDistributionPage = () => {
-  console.log('render')
   return (
     <>
       <Navbar />
-      <DistributionEditForm />
+      <DistributionEditForm 
+        path="idealRouteGradesById"
+      />
     </>
   )
 }
