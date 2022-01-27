b3e98c4d254440beac1f9795db03cae6703b6ae2
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-27 13:48:31 -0500

    Refactor the DistrbutionEditForm class component to a functional component as well as remove hardcoded api calls for more dynamic ones

--- src/components/distributions/DistributionEditForm.js
+++ src/components/distributions/DistributionEditForm.js
@@ -1,83 +1,62 @@
-import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-class DistributionEditForm extends React.Component {
-  constructor(props) {
-    super(props)
+const DistributionEditForm = (props) => {
+  const urlParams = useParams()
+  const [distributionSpread, setDistributionSpread] = useState({})
+  const [gymId, setGymId] = useState(0)
 
-    this.state = {
-      distribution: {
-        type: 'boulders',
-        gymId: 1,
-        distributionList: {
-          'VB': 4,
-          'V0': 5,
-          'V1': 5,
-          'V2': 4,
-          'V3': 6,
-          'V4': 5,
-          'V5': 4,
-          'V6': 7,
-          'V7': 9,
-          'V8': 9,
-          'V9': 8,
-          'V10': 6,
-          'V11': 2,
-          'V12': 1,
-          'V13': 0,
-          'V14': 0,
-          'V15': 0,
-          'V16': 0
-        }
-      }
+  useEffect(() => {
+    const getInfo = async () => {
+      const { data } = await axios.get(`http://localhost:1337/api/${props.path}/${urlParams.id}`)
+      const {gymId, gym, ...rest } = data
+
+      setDistributionSpread(rest)
+      setGymId(gymId)
     }
 
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
-  }
+    getInfo()
+  }, [urlParams, props.path])
 
-  handleChange(event) {
-    this.setState({
-      distribution: {
-        ...this.state.distribution,
-        distributionList: {
-          ...this.state.distribution.distributionList,
-          [event.target.name]: parseInt(event.target.value) || 0
-        }
-      }
+  const handleChange = (event) => {
+    setDistributionSpread({
+      ...distributionSpread,
+      [event.target.name]: parseInt(event.target.value) || 0
     })
   }
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault()
   }
 
-  render() {
-    return (
-      <form action={`/api/saveDistribution/${this.state.distribution.type}`} method="post" id="distribution-form" >
-        <input className="hidden" name="gymId" value={this.state.distribution.gymId} disabled />
-        <div className="grid">
-          {
-            Object.keys(this.state.distribution.distributionList).map(grade => {
-              let numberOfGrade = this.state.distribution.distributionList[grade]
-              return (
-                <div key={grade} className="distribution-form-div">
-                  <label className="distribution-form-label" htmlFor={grade}>{grade}:</label>
-                  <input
-                    onChange={this.handleChange}
-                    className="centered-text"
-                    style={{width: '2rem'}}
-                    name={grade}
-                    type="number"
-                    value={numberOfGrade}
-                  />
-                </div>
-              )
-            })
-          }
-        </div>
-                    <button onClick={this.handleSubmit} type="submit">Save Distribution</button>
-      </form>
-    )
-  }
+  return (
+    <form id="distribution-form" >
+      <input className="hidden" name="gymId" value={gymId} disabled />
+      <div className="grid">
+        {
+          Object.keys(distributionSpread).map(grade => {
+            const formattedGrade = grade.replace('_', '.')
+            let numberOfGrade = distributionSpread[grade]
+            return (
+              <div key={grade} className="distribution-form-div">
+                <label className="distribution-form-label" htmlFor={grade}>{formattedGrade}:</label>
+                <input
+                  onChange={handleChange}
+                  className="centered-text"
+                  style={{width: '2rem'}}
+                  name={grade}
+                  type="number"
+                  value={numberOfGrade}
+                />
+              </div>
+            )
+          })
+        }
+      </div>
+                  <button onClick={handleSubmit} type="submit">Save Distribution</button>
+    </form>
+  )
 }
+
 export default DistributionEditForm
