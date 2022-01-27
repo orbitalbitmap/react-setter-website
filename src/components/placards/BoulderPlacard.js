ae53c9f683a59b08ec6cbf7b3584f834aea94295
orbitalbitmap <orbitalbitmap@gmail.com>
2022-01-27 18:58:08 -0500

    Refactor the BoulderPlacard class component to a functional component

--- src/components/placards/BoulderPlacard.js
+++ src/components/placards/BoulderPlacard.js
@@ -2,43 +2,41 @@ import React from 'react'
 
 import BoulderSlot from './BoulderSlot'
 
-class BoulderPlacard extends React.Component {
-  render() {
-    let slotId = 0
-    return (
-      <div className="boulder-placard-container">
-        <div className="boulder-grid-three-column">
-          {
-            this.props.climbs.map(climb => {
-              slotId++
-              return (
-                <BoulderSlot
-                  key={slotId}
-                  slotId={slotId}
-                  color={climb.color?.toLowerCase()}
-                  grade={climb.grade}
-                  dateSet={climb.dateSet}
-                  arete={climb.message}
-                  setter={climb.setter}
-                />
-              )
-            })
-          }
-          </div>
-          
+const BoulderPlacard = (props) => {
+  let slotId = 0
+  return (
+    <div className="boulder-placard-container">
+      <div className="boulder-grid-three-column">
+        {
+          props.climbs.map(climb => {
+            slotId++
+            return (
+              <BoulderSlot
+                key={slotId}
+                slotId={slotId}
+                color={climb.color?.toLowerCase()}
+                grade={climb.grade}
+                dateSet={climb.dateSet}
+                arete={climb.areteMessage}
+                setter={climb.setter}
+              />
+            )
+          })
+        }
+        </div>
+        
 
-          <div className="boulder-social-grid">
-            <div className="boulder-placard-images">
-              <img className="boulder-crg-logo" src="/images/CRG_Logo_Text_M.jpeg" alt="CRG Logo"/>
-            </div>
-            <div className="boulder-placard-images boulder-social-media">
-              <img className="boulder-instagram-logo" src="/images/IG_logo.png" alt="Instagram Logo"/>
-              <p className="boulder-insta-handle">@crgworcester</p>
-            </div>
+        <div className="boulder-social-grid">
+          <div className="boulder-placard-images">
+            <img className="boulder-crg-logo" src="/images/CRG_Logo_Text_M.jpeg" alt="CRG Logo"/>
+          </div>
+          <div className="boulder-placard-images boulder-social-media">
+            <img className="boulder-instagram-logo" src="/images/IG_logo.png" alt="Instagram Logo"/>
+            <p className="boulder-insta-handle">@crgworcester</p>
           </div>
         </div>
-    )
-  }
+      </div>
+  )
 }
 
 export default BoulderPlacard
