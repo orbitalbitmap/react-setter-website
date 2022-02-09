4dc3b4ecb05eab3972951108b28358f2710bc2cd
orbitalbitmap <orbitalbitmap@gmail.com>
2022-02-09 07:54:39 -0500

    Refactor the helper/bcrypt file to use import/exports

--- src/helpers/bcrypt.js
+++ src/helpers/bcrypt.js
@@ -1,19 +1,14 @@
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 
-const hashPassword = async (pass) => {
+export const hashPassword = async (pass) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(pass, salt);
 
   return hashedPassword;
 };
 
-const checkPass = async (enteredPassword, hashedPassword) => {
+export const checkPass = async (enteredPassword, hashedPassword) => {
   const doesMatch = await bcrypt.compare(enteredPassword, hashedPassword)
 
   return doesMatch
-};
-
-module.exports = {
-  hashPassword,
-  checkPass
 };
