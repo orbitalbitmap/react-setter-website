65431dc1c95979c2c18e75255fb57541db6b9803
Rob Perron <orbitalbitmap@gmail.com>
2022-03-13 13:30:27 -0400

    Update the Login component to use MUI

---
+++ src/components/login/copyright/Copyright.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="http://robert-perron.com">
+        robert-perron.com
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default Copyright
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import '../components/styles.css'
-import Login from '../components/Login'
+import Login from '../components/login/Login'
 
 const LoginPage = () => {
   return (
