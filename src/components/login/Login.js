364a59424493692936a112fa9059c2ea75b3b58e
Rob Perron <orbitalbitmap@gmail.com>
2022-03-13 13:30:27 -0400

    Update the Login component to use MUI

---
+++ src/components/login/Login.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Cookies } from 'react-cookie'
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+import Copyright from './copyright/Copyright';
+import { getLocations, signIn } from '../../actions'
+
+const { checkPass } = require('../../helpers/bcrypt');
+const theme = createTheme();
+
+const SignIn = (props) => {
+  const cookies = new Cookies()
+  const navigate = useNavigate()
+  const [enteredEmail, setEnteredEmail] = useState('')
+  const [enteredPassword, setEnteredPassword] = useState('')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    if (enteredPassword.length <= 0 || enteredEmail.length <= 0) {
+      window.alert("password and email are required")
+      return
+    }
+
+    const {password, ...user} = (await axios.get(`${process.env.REACT_APP_API_PATH}/employeeByEmail/${enteredEmail}`)).data
+    const passwordDoesMatch = await checkPass(enteredPassword, password);
+
+    switch (passwordDoesMatch) {
+      case true:
+        props.signIn(user)
+        props.getLocations()
+        cookies.set('setter', user, { path: '/' })
+        navigate('/dashboard', {replace: true})
+        break
+      case false:
+        console.log('faiulre')
+        break
+      default:
+        console.log('faiulre')
+        break
+    }
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: 8,
+          }}
+        >
+
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              autoComplete="email"
+              autoFocus
+              fullWidth
+              id="email"
+              label="Email Address"
+              margin="normal"
+              name="email"
+              onChange={(event) => setEnteredEmail(event.target.value)}
+              required
+              type="email"
+            />
+
+            <TextField
+              autoComplete="current-password"
+              fullWidth
+              id="password"
+              label="Password"
+              margin="normal"
+              name="password"
+              onChange={(event) => setEnteredPassword(event.target.value)}
+              required
+              type="password"
+            />
+
+            <Button
+              fullWidth
+              sx={{ mt: 3, mb: 2 }}
+              type="submit"
+              variant="contained"
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default connect(null, { getLocations, signIn })(SignIn)
