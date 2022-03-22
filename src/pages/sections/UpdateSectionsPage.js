3f11f3daff6d52dbb69aa32155feb75873fe74ae
Rob Perron <orbitalbitmap@gmail.com>
2022-03-22 01:05:25 -0400

    Adjust more spacing inconsitencies

--- src/pages/sections/UpdateSectionsPage.js
+++ src/pages/sections/UpdateSectionsPage.js
@@ -5,7 +5,7 @@ import Dashboard from '../../components/dashboard/Dashboard'
 const UpdateSectionsPage = () => {
   return (
     <Dashboard>
-        <UpdateClimbingSections /> 
+      <UpdateClimbingSections /> 
     </Dashboard>
   )
 }
