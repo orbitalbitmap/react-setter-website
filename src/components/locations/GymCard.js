8cf00bc65b2e459ad1ea233d62a285ed20ab87e9
Rob Perron <orbitalbitmap@gmail.com>
2022-04-19 11:04:50 -0400

    Upgrade the Gyms component to use Material UI

---
+++ src/components/locations/GymCard.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom'
+import { Grid, Paper, Typography, List, ListItem, ListItemText  } from '@mui/material'
+
+const GymCard = ({ gymInfo }) => {
+  return (
+    <Grid item sx={{ pb: 4 }}>
+      <Paper sx={{ width: '30rem', m: '0 auto', p: 0, borderRadius: 4, }}>
+        <Typography variant="h5" key={gymInfo.name} className="centered-text">
+          <Link to={`/locations/${gymInfo.id}`}>{gymInfo.name}</Link>
+          <List
+            sx={{ bgcolor: theme => theme.palette.common.white, }}
+          >
+            <ListItem sx={{ textAlign: 'center', }} >
+              <ListItemText
+                sx={{ color: theme => theme.palette.common.black, }}
+              >
+                Address: {gymInfo.address}
+              </ListItemText>
+            </ListItem>
+            <ListItem sx={{ textAlign: 'center', }} >
+              <ListItemText
+                sx={{ color: theme => theme.palette.common.black, }}
+              >
+                Phone Number: {gymInfo.phoneNumber}
+              </ListItemText>
+            </ListItem>
+          </List>
+        </Typography>
+      </Paper>
+    </Grid>
+  )
+}
+
+export default GymCard
--- src/components/locations/Gyms.js
+++ src/components/locations/Gyms.js
@@ -1,19 +1,50 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Box, Container, Grid, Typography } from '@mui/material'
+
+import GymCard from './GymCard'
 
 const Gyms = (props) => (
   <>
-    <h1 className="centered-text">Locations:</h1>
-    {
-      props.gyms?.map(gym => {
-        return (
-          <h3 key={gym.name} className="centered-text">
-            <Link to={`/locations/${gym.id}`}>{gym.name}</Link>
-          </h3>
-        )
-      })
-    }
+    <Box
+      sx={{
+        bgcolor: theme => theme.palette.primary.main,
+        height: '50rem',
+        m: '8rem auto',
+        maxWidth: '75rem',
+        px: 2,
+        borderRadius: 4,
+      }}
+    >
+      <Typography
+        variant="h3"
+        className="centered-text"
+        sx={{
+          py: 2,
+          color: theme => theme.palette.primary.contrastText
+        }}
+      >
+        Locations:
+      </Typography>
+      <Container
+        style={{ overflowY: 'scroll', height: '40rem', }}
+        sx={{
+          color: theme => theme.palette.primary.contrastText,
+          bgcolor: theme => theme.palette.primary.light,
+          m: '0 auto',
+          borderRadius: 4,
+        }}>
+        <Grid container xs={12} spacing={8} sx={{ m: '0 auto', }}>
+          {
+            props.gyms?.map(gym => {
+              return (
+                <GymCard key={gym.name} gymInfo={gym} />
+              )
+            })
+          }
+        </Grid>
+      </Container>
+    </Box>
   </>
 )
 
