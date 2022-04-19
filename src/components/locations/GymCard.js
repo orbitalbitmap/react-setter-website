071371d72a64fb5317b4d209c0d29ef4f16ef041
Rob Perron <orbitalbitmap@gmail.com>
2022-04-19 18:49:12 -0400

    Fix The typography component to only be around the link component

--- src/components/locations/GymCard.js
+++ src/components/locations/GymCard.js
@@ -7,25 +7,25 @@ const GymCard = ({ gymInfo }) => {
       <Paper sx={{ width: '30rem', m: '0 auto', pb: 2, }}>
         <Typography variant="h5" key={gymInfo.name} className="centered-text" sx={{pt: 2}}>
           <Link to={`/locations/${gymInfo.id}`}>{gymInfo.name}</Link>
-          <List
-            sx={{ bgcolor: theme => theme.palette.common.white, }}
-          >
-            <ListItem sx={{ textAlign: 'center', }} >
-              <ListItemText
-                sx={{ color: theme => theme.palette.common.black, }}
-              >
-                Address: {gymInfo.address}
-              </ListItemText>
-            </ListItem>
-            <ListItem sx={{ textAlign: 'center', }} >
-              <ListItemText
-                sx={{ color: theme => theme.palette.common.black, }}
-              >
-                Phone Number: {gymInfo.phoneNumber}
-              </ListItemText>
-            </ListItem>
-          </List>
         </Typography>
+        <List
+          sx={{ bgcolor: theme => theme.palette.common.white, }}
+        >
+          <ListItem sx={{ textAlign: 'center', }} >
+            <ListItemText
+              sx={{ color: theme => theme.palette.common.black, }}
+            >
+              Address: {gymInfo.address}
+            </ListItemText>
+          </ListItem>
+          <ListItem sx={{ textAlign: 'center', }} >
+            <ListItemText
+              sx={{ color: theme => theme.palette.common.black, }}
+            >
+              Phone Number: {gymInfo.phoneNumber}
+            </ListItemText>
+          </ListItem>
+        </List>
       </Paper>
     </Grid>
   )
