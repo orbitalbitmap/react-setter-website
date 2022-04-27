ba8cecc8c705a8a59ff8da386052bc4ecebcdbd9
Rob Perron <orbitalbitmap@gmail.com>
2022-04-27 08:59:33 -0400

    Fix the EmployeeCardContainer's mui error

--- src/components/employees/EmployeeCardContainer.jsx
+++ src/components/employees/EmployeeCardContainer.jsx
@@ -11,12 +11,12 @@ export const getLocationString = (employeeLocations) => {
 
 const EmployeeCardContainer = (props) => {
   return (
-    <Grid key="employee-card-list-container" container xs={12} spacing={8} sx={{ m: '0 auto', }}>
+    <Grid key="employee-card-list-container" container sx={{ m: '0 auto', }}>
       {
         props?.employees.map(employeeInfo => {
           return (
-            <Grid key={employeeInfo?.id} item sx={{ pb: 4 }}>
-              <Paper sx={{ width: '30rem', m: '0 auto', pb: 2, }}>
+            <Grid key={employeeInfo?.id} item xs={6} sx={{ py: 2 }}>
+              <Paper sx={{ width: '30rem', m: '0 auto', pb: 4, }}>
                 <Typography variant="h5" className="centered-text" sx={{pt: 2}}>
                   <Link to={`/employees/${employeeInfo?.id}`}>{`${employeeInfo?.firstName} ${employeeInfo?.lastName}`}</Link>
                 </Typography>
