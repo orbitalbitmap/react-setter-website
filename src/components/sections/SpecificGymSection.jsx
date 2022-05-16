d72e4d8edb5bdcb848a6930d7ebed5f5baf316e9
Rob Perron <orbitalbitmap@gmail.com>
2022-05-16 12:59:05 -0400

    Convert the SpecificGymSection component to jsx file and update it's ui with material-ui

---
+++ src/components/sections/SpecificGymSection.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import SectionCardsContainer from './SectionCardsContainer';
+
+
+const SpecificGymSection = () => {
+  const urlParams = useParams()
+  const [gym, setGym] = useState({})
+
+  const renderInfo = () => {
+    return (
+      <div style={{ margin: '5rem auto 0', width: '50%', }}>
+        <SectionCardsContainer
+          key={gym.id}
+          boulderSections={gym.boulderSections}
+          routeSections={gym.routeSections}
+          gymName={gym.name}
+          gymId={gym.id}
+        />
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const { data } = await axios.get(`${process.env.REACT_APP_API_PATH}/gymWithSections/${urlParams.id}`)
+      setGym(data)
+    }
+
+    getInfo()
+  }, [urlParams])
+
+  return gym.id 
+    ?  renderInfo()
+    : null
+}
+
+export default SpecificGymSection
