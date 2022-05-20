dfcea29970b5a43d7fa866ca1c96049a8ecac283
Rob Perron <orbitalbitmap@gmail.com>
2022-05-20 01:04:41 -0400

    Update the SectionList component to ise material ui and change to a jsx file

---
+++ src/components/distributions/SectionsList.jsx
@@ -0,0 +1,48 @@
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import React from 'react'
+
+const SectionsList = (props) => {
+  return (
+  <Box>
+    <FormControl>
+      <InputLabel>Section</InputLabel>
+      <Select
+        label="Section"
+        labelId="Section"
+        style={{ color: theme => theme.palette.common.black }}
+        value={props.currentSelectedId}
+      >
+        <MenuItem
+          key={0}
+          id={0}
+          value={0}
+          onClick={props.onClick}
+          sx={{ color: theme => theme.palette.common.white }}
+        >
+          Please select a section
+        </MenuItem>
+        { 
+          props.sectionList?.map(section => {
+            const isSelected = props.currentSelectedId === section.id ? 'selected' : null
+            const sectionId = section.id;
+            return (
+              <MenuItem
+                key={section.id}
+                id={section.id}
+                value={sectionId}
+                className={`section-selectors ${isSelected}`}
+                onClick={props.onClick}
+                sx={{ color: theme => theme.palette.common.white }}
+              >
+                {section?.name}
+              </MenuItem>
+            )
+          })
+        }
+      </Select>
+    </FormControl>
+  </Box>
+  )
+}
+
+export default SectionsList
