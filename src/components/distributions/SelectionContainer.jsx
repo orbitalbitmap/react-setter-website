f90a40769add3be58a60d0b1c49262ae9665c7b6
Rob Perron <orbitalbitmap@gmail.com>
2022-05-21 22:46:20 -0400

    Update the RouteDistributionChart component to use material ui and change to a jsx file

--- src/components/distributions/SelectionContainer.jsx
+++ src/components/distributions/SelectionContainer.jsx
@@ -18,14 +18,19 @@ function SelectionContainer(props) {
       break;
   }
 
+  const styles = props.sx
+  ? { maxWidth: '12rem', minWidth: '5rem', ...props.sx }
+  : { maxWidth: '12rem', minWidth: '5rem', }
+
+
   return (
-    <FormControl sx={{ maxWidth: '15rem', }}>
-      <InputLabel sx={{ color: fontColor, }}>{props.name}</InputLabel>
+    <FormControl sx={styles}>
+      <InputLabel sx={{ color: fontColor, width: props.sx ? props.sx.width : 'inherit'}}>{props.name === 'sectionId' ? 'Location' : props.name}</InputLabel>
       <Select
         label={props.name}
         classes={{select: props.color.toLowerCase}}
         value={props.value}
-        sx={{ color: fontColor }}
+        sx={{ color: fontColor, ...props.sx }}
       >
         {
           props.list?.map((item) => {
@@ -36,7 +41,7 @@ function SelectionContainer(props) {
                 component = <MenuItem sx={{ color: theme => theme.palette.primary.contrastText }} key={`${props.name}-${item}-${props.id}`} onClick={handleChange} className={`climb${props.id} ${props.color.toLowerCase()}`} id={props.name} data-index={props.id-1} data-name={props.name} value={item}>{item}</MenuItem>
                 break;
               case 'ropeStyle':
-                component = (<MenuItem sx={{ color: theme => theme.palette.primary.contrastText }} key={`${props.name}-${item}-${props.id}`} className={`climb${props.id} ${props.color.toLowerCase()}`} onClick={props.onChange} id={props.name} data-index={props.id-1} data-name={props.name} value={item[props.valueKey]}>{item}</MenuItem>);
+                component = (<MenuItem sx={{ color: theme => theme.palette.primary.contrastText }} key={`${props.name}-${item}-${props.id}`} className={`climb${props.id} ${props.color.toLowerCase()}`} onClick={handleChange} id={props.name} data-index={props.id-1} data-name={props.name} value={item}>{item}</MenuItem>);
                 break;
               case 'setter':
                 component = item.placardName
