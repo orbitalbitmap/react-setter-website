60de5024a59133e0d4e980980180b59e86b613d7
Rob Perron <orbitalbitmap@gmail.com>
2023-04-19 08:45:08 +0100

    Update the base ul for rtk query to use the environment variable

--- src/services/gym.js
+++ src/services/gym.js
@@ -1,10 +1,12 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const baseUrl = process.env.REACT_APP_API_PATH;
 
 // Define a service using a base URL and expected endpoints
 export const gymApi = createApi({
   reducerPath: 'gymApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1337/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     // queries
     getAllLocations: builder.query({
@@ -26,7 +28,7 @@ export const gymApi = createApi({
       }),
     }),
   })
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
