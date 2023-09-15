b9135a0e92a1ce6db3bb5a9f10e3103ad4538e9b
Rob Perron <orbitalbitmap@gmail.com>
2023-09-15 16:16:51 -0400

    Add unit test for the getClimbListForPlacard function

---
+++ src/tests/unit/printableBoulderPlacard.test.js/getClimbListForPlacard.test.js
@@ -0,0 +1,32 @@
+import getClimbListForPlacard from "../../../components/placards/printableBoulderPlacard/utils/getClimbListForPlacard";
+
+describe("getClimbListForPlacard function", () => {
+  it("returns the expected result when passed valid inputs", () => {
+    const startNumber = 1;
+    const endNumber = 3;
+
+    const actualResult = getClimbListForPlacard(startNumber, endNumber);
+    const expectedResult = ["climb1", "climb2", "climb3"];
+
+    expect(actualResult).toEqual(expectedResult);
+  });
+  it("returns an empty array when passed no inputs", () => {
+    const actualResult = getClimbListForPlacard();
+    const expectedResult = [];
+
+    expect(actualResult).toEqual(expectedResult);
+  }); 
+  it("returns an empty array when passed string inputs", () => {
+    const actualResult = getClimbListForPlacard('1', '2');
+    const expectedResult = [];
+
+    expect(actualResult).toEqual(expectedResult);
+  });
+
+  it("returns an empty array when passed boolean value inputs", () => {
+    const actualResult = getClimbListForPlacard(true, false);
+    const expectedResult = [];
+
+    expect(actualResult).toEqual(expectedResult);
+  });
+});
