ea4f9c45e0744464f2870d97049958d63bf82fa1
Rob Perron <orbitalbitmap@gmail.com>
2023-10-19 22:13:42 -0400

    Remove unnecessary import in the api file

--- src/services/gym.js
+++ src/services/gym.js
@@ -1,7 +1,6 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setEmployeeList } from '../reducers/employeeReducers';
-import { setUser } from '../reducers/userReducer';
 
 const baseUrl = process.env.REACT_APP_API_PATH;
 
