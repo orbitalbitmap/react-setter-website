781d6e6c5683fcfa01f9d6f17a72739fb143fa09
Rob Perron <orbitalbitmap@gmail.com>
2023-12-31 21:05:48 -0500

    Add an e2e test for the IdealRouteDistribution page

---
+++ src/tests/e2e/IdealRouteDistribution.spec.js
@@ -0,0 +1,14 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import { test, expect } from '@playwright/test';
+
+
+// render test
+test('loads expected content', async ({page}) => {
+  await page.goto('distribution/ideal/ropes/1');
+  
+  const form = page.getByTestId('routes-distribution-form');
+  const saveButton = page.getByText('Save Distribution')
+  
+  await expect(form).toBeVisible();
+  await expect(saveButton).toBeVisible();
+});
