f7e1ba859e79cfb83eb8c04ac2088574a1c66080
Rob Perron <orbitalbitmap@gmail.com>
2024-01-03 03:36:12 -0500

    Remove redundant e2e AdminDashboard tests

--- src/tests/e2e/admin/AdminDashboard.spec.js
+++ src/tests/e2e/admin/AdminDashboard.spec.js
@@ -15,33 +15,6 @@ test.beforeEach('mock the necessary api paths before navigating to the AdminDash
 })
 
 // render test
-test('load expected content', async ({page}) => {
-  const newEmployeeTab = page.getByTestId('new-setter-tab');
-  const newEmployeeFrom = page.getByTestId('new-setter-form');
-  const newGymTab = page.getByTestId('new-gym-tab');
-  const newGymForm = page.getByTestId('new-gym-form');
-
-  await expect(newEmployeeTab).toBeVisible();
-  await expect(newEmployeeFrom).toBeVisible();
-  await expect(newGymTab).toBeVisible();
-  await expect(newGymForm).not.toBeVisible();
-});
-
-test('switch the visible content from the new setter form to the new gym form and then back', async ({ page }) => {
-  const newEmployeeTab = page.getByTestId('new-setter-tab');
-  const newEmployeeFrom = page.getByTestId('new-setter-form');
-  const newGymTab = page.getByTestId('new-gym-tab');
-  const newGymForm = page.getByTestId('new-gym-form');
-
-  await newGymTab.click();
-  await expect(newEmployeeFrom).not.toBeVisible();
-  await expect(newGymForm).toBeVisible();
-
-  await newEmployeeTab.click();
-  await expect(newEmployeeFrom).toBeVisible();
-  await expect(newGymForm).not.toBeVisible();
-});
-
 test('make sure the new employee form works as expected', async ({ page }) => {
   await page.route('*/**/api/saveEmployee', async route => {
     await route.fulfill({ status: 200});
