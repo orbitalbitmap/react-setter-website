322876abde664a0c791a6e091704b09e645fc54a
Rob Perron <orbitalbitmap@gmail.com>
2024-01-04 08:04:15 -0500

    Remove the unnecessary test.beforeEach

--- src/tests/e2e/locations/Gyms.spec.js
+++ src/tests/e2e/locations/Gyms.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import mockGymList from '../../mock-data/mockGymList';
 import mockEmployeeList from '../../mock-data/mockEmployeeList';
 
-test.beforeEach('mock out the appropriate api paths before navigating to the Gyms page', async ({ page }) => {
+test('loads expected content', async ({ page }) => {
   await page.route('*/**/api/employees', async route => {
     await route.fulfill({ json: mockEmployeeList });
   });
@@ -12,8 +12,7 @@ test.beforeEach('mock out the appropriate api paths before navigating to the Gym
   });
 
   await page.goto('locations');
-});
-test('loads expected content', async ({ page }) => {
+
   const mainContainer = page.getByTestId('main-container');
   const locationsContainer = page.getByTestId('location-cards-container');
   const locationCardList = page.getByTestId('location-card');
@@ -23,7 +22,7 @@ test('loads expected content', async ({ page }) => {
   await expect(mainContainer).toBeVisible();
   await expect(locationsContainer).toBeVisible();
   await expect(locationCardList).toHaveCount(mockGymList.length);
-  
+
   await expect(firstCard.getByRole('heading')).toHaveText(mockGymList[0].name);
   await expect(firstCard).toContainText(mockGymList[0].address);
   await expect(firstCard).toContainText(mockGymList[0].phoneNumber);
